df8e05f00283e7dbcd9b9fc2f05b622f3d7619b2
Gireesh <giri4it@gmail.com>
2023-07-10 13:48:15 +0530

    first I created a sum and First java files

---
+++ com/example/First.java
@@ -0,0 +1,17 @@
+package com.example;
+
+public class First {
+
+    //byte a2b2 = 255; //8 bits -> 256
+    //char ch1;  // 16 its -> 
+    int abc; // 32 bits - 2^16 -> 160000
+    //float f1; 16 bits Mantizza, 16 bits for fraction 2.4
+    //double db1; 32 bits
+    //String str = "hello"; //collection of chars \0
+
+
+    public static void main(String[] args){
+        System.out.println("hello");
+    }
+
+}
---
+++ com/example/Sum.java
@@ -0,0 +1,18 @@
+package com.example;
+
+public class Sum {
+     static int  a = 10;
+     static int b = 23;
+
+    public static int add(){
+
+       int sum ;
+       sum = a + b; 
+       return sum;
+    }
+
+    public static void main(String[] args) {
+       System.out.println(add()); 
+    }
+
+}

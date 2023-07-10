8ac2d6365c467b65fbe6b737f4ea806e82e5fb7e
Gireesh <giri4it@gmail.com>
2023-07-10 13:52:15 +0530

    removed comments from first.java

--- com/example/First.java
+++ com/example/First.java
@@ -2,12 +2,12 @@ package com.example;
 
 public class First {
 
-    //byte a2b2 = 255; //8 bits -> 256
-    //char ch1;  // 16 its -> 
-    int abc; // 32 bits - 2^16 -> 160000
+    //byte a2b2 = 255; 
+    //char ch1;  
+    int abc; 
     //float f1; 16 bits Mantizza, 16 bits for fraction 2.4
     //double db1; 32 bits
-    //String str = "hello"; //collection of chars \0
+    //String str = "hello"; 
 
 
     public static void main(String[] args){

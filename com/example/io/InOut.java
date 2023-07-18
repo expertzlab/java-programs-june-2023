a5e209ee99f129f8a0476612f4719d9bf3429041
Gireesh <giri4it@gmail.com>
2023-07-18 15:10:58 +0530

    name sacnning version

--- com/example/io/InOut.java
+++ com/example/io/InOut.java
@@ -5,13 +5,13 @@ import java.util.Scanner;
 public class InOut {
 
     public static void main(String[] args) {
-        System.out.println("Enter a number");
+        System.out.println("Enter first name:");
 
-        int a = new Scanner(System.in).nextInt();
-        System.out.println("Enter the next number");
-        int b = new Scanner(System.in).nextInt();
+        String fn = new Scanner(System.in).next();
+        System.out.println("Enter last name:");
+        String ln = new Scanner(System.in).next();
 
-        System.out.println("sum="+ (a+b));
+        System.out.println("your full name is"+fn +" "+ln);
     }
     
 }
